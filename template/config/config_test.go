a349fdff99fdd2cd42721f3a33e399b41eab5e8e
sakchai.pa <sakchai.pa@arise.tech>
2026-03-30 01:51:50 +0700

    :white_check_mark: update unit test

--- template/config/config_test.go
+++ template/config/config_test.go
@@ -3,7 +3,6 @@ package config
 import (
 	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -16,32 +15,14 @@ func TestLoad(t *testing.T) {
 		t.Setenv("APP_NAME", "TestApp")
 		t.Setenv("APP_PORT", "8080")
 		t.Setenv("APP_VERSION", "1.0.0")
-		t.Setenv("DATABASE_URL", "Use Here")
-		t.Setenv("LOCAL_DATABASE_URL", "Not Use Here")
-		t.Setenv("LOCAL_REDIS_HOST", "Use Here")
 
-		expectConfig := Config{
-			App: App{
-				Name:    "TestApp",
-				Port:    "8080",
-				Version: "1.0.0",
-			},
-			Header: Header{
-				RefIDKey: "X-Ref-ID",
-			},
-			Database: Database{
-				URL: "Use Here",
-			},
-			Migration: Migration{
-				Directory: "./migrations",
-			},
-			Redis: Redis{
-				Host:    "Use Here",
-				Timeout: time.Minute * 10,
-			},
+		expectConfig := App{
+			Name:    "TestApp",
+			Port:    "8080",
+			Version: "1.0.0",
 		}
 
 		cfg := Load(Env)
-		assert.Equal(t, expectConfig, cfg)
+		assert.Equal(t, expectConfig, cfg.App)
 	})
 }
